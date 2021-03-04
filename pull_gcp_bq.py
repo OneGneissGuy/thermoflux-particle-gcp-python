8141134dba87d63265eacdb5c10d1feac18f95e3
JohnFranco Saraceno <jfsaraceno@gmail.com>
2021-03-04 12:23:04 -0800

    drop dups in db and change col names

--- pull_gcp_bq.py
+++ pull_gcp_bq.py
@@ -44,7 +44,7 @@ credentials_file = "bq_service_account.json"
 # GCP BIQ QUERY FILE INFO
 bq_filename = "demo_dataset.demo_table"
 ts_index_column = "ancillaryTimeStamp"
-deployment_date = "11-17-2020"
+deployment_date = "02-26-2020"
 
 service_account_info = json.load(open(credentials_file))
 credentials = service_account.Credentials.from_service_account_info(
@@ -61,7 +61,11 @@ ancillary_sql = """
  WHERE (device_id = "e00fce68e84fc8ca3d4edf94" AND temperature IS NOT NULL);
  """
 # query ancillary table data from BiqQuery and store it in a pandas dataframe
-df_ancillary_raw = client.query(ancillary_sql).to_dataframe()
+df_ancillary_raw = (
+    client.query(ancillary_sql)
+    .to_dataframe()
+    .drop_duplicates(subset=["ancillaryTimeStamp"])
+)
 df_ancillary_raw_sorted = format_dataframe(
     df_ancillary_raw, "ancillaryTimeStamp", drop_null=False, round_interval=True
 )
@@ -81,7 +85,9 @@ flux_sql = """
  WHERE (device_id = "e00fce68e84fc8ca3d4edf94" AND flux IS NOT NULL);
  """
 # query flux table data from BiqQuery and store it in a pandas dataframe
-df_flux_raw = client.query(flux_sql).to_dataframe()
+df_flux_raw = (
+    client.query(flux_sql).to_dataframe().drop_duplicates(subset=["fluxTimeStamp"])
+)
 
 df_flux_raw_sorted = format_dataframe(
     df_flux_raw, "fluxTimeStamp", drop_null=False, round_interval=False
@@ -106,10 +112,10 @@ df = df.round(decimals=2)
 # add units to plot cols
 df.rename(
     columns={
-        "battery": "battery (V)",
-        "temperature": "temperature (C)",
-        "netRadiation": "netRadiation (W/m2)",
-        "flux": "flux (W/m2)",
+        "battery": "Battery voltage (V)",
+        "temperature": "Temperature (C)",
+        "netRadiation": "Net Radiation (W/m2)",
+        "flux": "Sensible heat flux (W/m2)",
     },
     inplace=True,
 )
@@ -118,19 +124,19 @@ df.to_csv("demo-table-export-30min-ave.csv")
 
 # plot the data
 plt.figure()
-df.battery.plot(marker="o")
+df["Battery voltage (V)"].plot(marker="o")
 plt.ylim(12, 16)
-plt.ylabel("Battery voltage (V)")
+# plt.ylabel("Battery voltage (V)")
 plt.figure()
-df.temperature.plot(marker="o")
-plt.ylabel("Temperature (C)")
+df["Temperature (C)"].plot(marker="o")
+# plt.ylabel("Temperature (C)")
 plt.ylim(-5, 30)
 plt.figure()
-df.netRadiation.plot(marker="o")
+df["Net Radiation (W/m2)"].plot(marker="o")
 plt.ylabel(r"Net radiation, $R_{net}$ $\mathregular{(\frac{W}{m^{2}})}$")
 # plt.ylim(-100, 700)
 plt.figure()
-df.flux.plot(marker="o")
+df["Sensible heat flux (W/m2)"].plot(marker="o")
 plt.ylabel(r"Sensible heat flux, $H_{SR}$ $\mathregular{(\frac{W}{m^{2}})}$")
 plt.ylim(-200, 400)
 # count the number of samples in a day (should be 287-288 for ancillary; 47-48 for unfiltered flux)
